b4194b234f909e9f7ff69be2e8f0486ca9f2a0cf
Marcos Vinicius <viniciusramoscaldas@gmail.com>
2022-12-20 21:10:27 -0300

    Base do projeto.

---
+++ JNotes/src/main/java/com/marcosvr/jnotes/JNotes.java
@@ -0,0 +1,12 @@
+package com.marcosvr.jnotes;
+
+/**
+ *
+ * @author MARCOS VINICIUS
+ */
+public class JNotes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
