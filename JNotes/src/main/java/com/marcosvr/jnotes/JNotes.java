56a512c51741f8dd2f50d1b466742753acd2d7c2
Marcos Vinicius <viniciusramoscaldas@gmail.com>
2022-12-22 23:32:32 -0300

    Tela Principal

--- JNotes/src/main/java/com/marcosvr/jnotes/JNotes.java
+++ JNotes/src/main/java/com/marcosvr/jnotes/JNotes.java
@@ -1,5 +1,7 @@
 package com.marcosvr.jnotes;
 
+import com.marcosvr.view.TelaPrincipalView;
+
 /**
  *
  * @author MARCOS VINICIUS
@@ -7,6 +9,7 @@ package com.marcosvr.jnotes;
 public class JNotes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        TelaPrincipalView tpv = new TelaPrincipalView();
+        tpv.setVisible(true);
     }
 }
